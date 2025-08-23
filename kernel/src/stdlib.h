7ae103dccd2d9a232ab6e5c5ad26350810cf57c4
beccauwu <rebeccaperttula@gmail.com>
2025-08-23 13:43:12 +0200

    move mem* functions to stdlib

---
+++ kernel/src/stdlib.h
@@ -0,0 +1,19 @@
+#ifndef _STDLIB_H
+#define _STDLIB_H
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+
+size_t strlen(const char *s);
+size_t strnlen(const char *s, size_t maxlen);
+
+void *memcpy(void *restrict dest, const void *restrict src, size_t n);
+void *memset(void *s, int c, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+
+// Halt and catch fire function.
+extern void hcf(void);
+
+#endif // _STDLIB_H
